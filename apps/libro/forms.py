870a40813ece861093b65a67854b80c0d211fc43
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-21 13:51:26 -0500

    creación del archivo forms.py

---
+++ apps/libro/forms.py
@@ -0,0 +1,9 @@
+from django import form
+#cuando se encuentra el archivo dentro de la carpeta se llama con el . 
+from .models import Autor
+
+#sistema de plantillas automatico del formulario
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model = Autor
+        fields = ['nombre','apellidos','nacionalidad','descripcion']
