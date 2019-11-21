5e5fda2220666249bbc4c923be978665606d79a4
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-20 22:51:42 -0500

    modelo de Autor creado

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    id = models.AutoField(primary_key= True)
+    #blank False = No permitira que sea guardado en blanco, null False = no recibe campos vacios
+    nombre = models.CharField(max_length=50,blank=False,null=False)
+    apellidos = models.CharField(max_length=200, blank=False,null=False)
+    nacionalidad = models.CharField(max_length=50,blank=False,null=False)
+    descripcion = models.TextField(blank=False,null=False)
