d0acee4cc6dd15e0fa372f4ae4e232eec5380a71
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-21 02:13:59 -0500

    Relaciones en los modelos

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Autor
+from .models import Autor,Libro
 
 # Register your models here.
 admin.site.register(Autor)
+admin.site.register(Libro)
--- apps/libro/models.py
+++ apps/libro/models.py
@@ -8,6 +8,7 @@ class Autor(models.Model):
     apellidos = models.CharField('Apellidos',max_length=200, blank=False,null=False)
     nacionalidad = models.CharField('Nacionalidad',max_length=50,blank=False,null=False)
     descripcion = models.TextField('Descripción',blank=False,null=False)
+    fecha_creacion = models.DateField('Fecha de creación', auto_now = True,auto_now_add=False)
 
     #usamos los metadatos
     class Meta:
@@ -19,4 +20,27 @@ class Autor(models.Model):
     #Cada vez que el objeto se liste nos muestre el campo nombre
     def __str__(self):
         return self.nombre
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField('Titulo',blank=False,null=False, max_length=50)
+    fecha_publicacion = models.DateField('Fecha de publicación', blank=False, null=False, auto_now=False, auto_now_add=False)
+    #cada vez que se cree se actualizara el campo
+    fecha_creacion = models.DateField('Fecha de creación', auto_now = True,auto_now_add=False)
     
+    #Relación uno a uno: un autor solo podra tiene un libro o sino pondra error
+    #autor_id = models.OneToOneField(Autor,on_delete = models.CASCADE)
+
+    #Relación uno a muchos: un autor podra tener varios libros pero se crearan en diferente registros
+    #autor_id = models.ForeignKey(Autor,on_delete = models.CASCADE)
+
+    #Relacion muchos a mucho: un libro puede estar escrito por muchos autores y un autor pueden escribir varios libros
+    autor_id = models.ManyToManyField(Autor)
+
+    class Meta:
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+        ordering = ['titulo']
+    
+    def __str__(self):
+        return self.titulo
