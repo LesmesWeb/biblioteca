c14ff147687067343da061b15f30f9815a8ad447
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-21 01:42:16 -0500

    models.py metadatos

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -4,7 +4,19 @@ from django.db import models
 class Autor(models.Model):
     id = models.AutoField(primary_key= True)
     #blank False = No permitira que sea guardado en blanco, null False = no recibe campos vacios
-    nombre = models.CharField(max_length=50,blank=False,null=False)
-    apellidos = models.CharField(max_length=200, blank=False,null=False)
-    nacionalidad = models.CharField(max_length=50,blank=False,null=False)
-    descripcion = models.TextField(blank=False,null=False)
+    nombre = models.CharField('Nombres',max_length=50,blank=False,null=False)
+    apellidos = models.CharField('Apellidos',max_length=200, blank=False,null=False)
+    nacionalidad = models.CharField('Nacionalidad',max_length=50,blank=False,null=False)
+    descripcion = models.TextField('Descripción',blank=False,null=False)
+
+    #usamos los metadatos
+    class Meta:
+        #Definimos como queremos que se visualice en el panel administrativo
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        ordering = ['nombre'] #atributo por el cualquiero ordenar
+    
+    #Cada vez que el objeto se liste nos muestre el campo nombre
+    def __str__(self):
+        return self.nombre
+    
