601c743cac1deec2af83c36f3c588c3e2436701c
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-24 16:28:19 -0500

    repath y url

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from .views import crearAutor,listarAutor
+from django.urls import path, re_path
+from .views import crearAutor, listarAutor
 
 urlpatterns = [
-    #path recibe: ruta url / función de la vista / nombre de la ruta
-    path('crear_autor/',crearAutor,name='crear_autor'),
-    path('listar_autor/',listarAutor,name='listar_autor'),
+    # path recibe: ruta url / función de la vista / nombre de la ruta
+    # path es la manera que se usa en django 2.0
+    path('crear_autor/', crearAutor, name='crear_autor'),
+    path('listar_autor/', listarAutor, name='listar_autor'),
+    # re_path trabaja con la libreria de regex
+    #re_path(r'^crear_autor/(?P<id>\d+)',crearAutor, name = "crear_autor")
+    # antiguamente se usaba url() para la versiones viejas de django
 ]
-
