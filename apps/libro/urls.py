e2587f0e129ea23b97526c6ca723808795c691ca
Dev-Lesmes <lesmes01@hotmail.com>
2019-11-25 00:54:46 -0500

    editar formulario por url funcionando http://localhost:8000/libro/editar_autor/8

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path
-from .views import crearAutor, listarAutor
+from .views import crearAutor, listarAutor, editarAutor
 
 urlpatterns = [
     # path recibe: ruta url / función de la vista / nombre de la ruta
     # path es la manera que se usa en django 2.0
     path('crear_autor/', crearAutor, name='crear_autor'),
     path('listar_autor/', listarAutor, name='listar_autor'),
+    path('editar_autor/<int:id>', editarAutor, name='editar_autor'),
+
     # re_path trabaja con la libreria de regex
     #re_path(r'^crear_autor/(?P<id>\d+)',crearAutor, name = "crear_autor")
     # antiguamente se usaba url() para la versiones viejas de django
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import AutorForm
 from .models import Autor
 # Create your views here.
-#request es el parametro que va obtener de la interacción del usuario
+# request es el parametro que va obtener de la interacción del usuario
+
+
 def Home(request):
-    return render(request,'index.html') 
+    return render(request, 'index.html')
+
 
 def crearAutor(request):
     if request.method == 'POST':
@@ -15,10 +18,24 @@ def crearAutor(request):
     else:
         autor_form = AutorForm()
 
+    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form})
 
-    return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
+# Vistas basadas en funciones
 
-#Vistas basadas en funciones
+
 def listarAutor(request):
     autores = Autor.objects.all()
-    return render(request,'libro/listar_autor.html',{'autores':autores})
+    return render(request, 'libro/listar_autor.html', {'autores': autores})
+
+
+def editarAutor(request, id):
+    # con get solo traemos el unico dato que encuentre de la base de datos
+    autor = Autor.objects.get(id=id)
+    if request.method == 'GET':
+        autor_form = AutorForm(instance=autor)
+    else:
+        autor_form = AutorForm(request.POST, instance=autor)
+        if autor_form.is_valid():
+            autor_form.save()
+        return redirect('index')
+    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form})
